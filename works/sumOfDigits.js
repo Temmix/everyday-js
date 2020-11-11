2855fca18c5dcbbb09c940130b1697c859f96187
Temi Makinde <Temi.Makinde@Gmail.com>
2020-11-11 15:07:31 +0000

    some changes

--- works/sumOfDigits.js
+++ works/sumOfDigits.js
@@ -6,23 +6,23 @@ continue reducing in this way until a single-digit number is produced. This is o
 
 Here's how it works:
 
-digital_root(16)
+digitalRoot(16)
 => 1 + 6
 => 7
 
-digital_root(942)
+digitalRoot(942)
 => 9 + 4 + 2
 => 15 ...
 => 1 + 5
 => 6
 
-digital_root(132189)
+digitalRoot(132189)
 => 1 + 3 + 2 + 1 + 8 + 9
 => 24 ...
 => 2 + 4
 => 6
 
-digital_root(493193)
+digitalRoot(493193)
 => 4 + 9 + 3 + 1 + 9 + 3
 => 29 ...
 => 2 + 9
